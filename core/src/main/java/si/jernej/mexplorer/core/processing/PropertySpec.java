503aae65f5584214c5c2b3096ec221d791a39d5b
Jernej Vivod <vivod.jernej@gmail.com>
2022-03-02 09:25:11 +0100

    [feat][#16]Implement class implementing functionality for specifying the properties of entities to include in the Wordification algorithm

--- core/src/main/java/si/jernej/mexplorer/core/processing/PropertySpec.java
+++ core/src/main/java/si/jernej/mexplorer/core/processing/PropertySpec.java
@@ -24,22 +24,34 @@ public class PropertySpec
      * Add entry to the specification.
      *
      * @param entity name of entity
-     * @param field name of the entity's property
+     * @param property name of the entity's property
      */
-    public void addEntry(String entity, String field)
+    public void addEntry(String entity, String property)
     {
-        entityToPropertiesToProcess.computeIfAbsent(entity, e -> new HashSet<>()).add(field);
+        entityToPropertiesToProcess.computeIfAbsent(entity, e -> new HashSet<>()).add(property);
     }
 
     /**
      * Add entries to the specification.
      *
      * @param entity name of entity
-     * @param fields name of entity's property
+     * @param properties name of entity's property
      */
-    public void addEntry(String entity, Collection<String> fields)
+    public void addEntry(String entity, Collection<String> properties)
     {
-        entityToPropertiesToProcess.computeIfAbsent(entity, e -> new HashSet<>()).addAll(fields);
+        entityToPropertiesToProcess.computeIfAbsent(entity, e -> new HashSet<>()).addAll(properties);
+    }
+
+    /**
+     * Check if property name for entity specified.
+     *
+     * @param entity name of entity
+     * @param propertyName name of entity's property
+     * @return is the entity's property specified to be part of the Wordification algorithm's output
+     */
+    public boolean containsEntry(String entity, String propertyName)
+    {
+        return this.entityToPropertiesToProcess.containsKey(entity) && this.entityToPropertiesToProcess.get(entity).contains(propertyName);
     }
 
 }
