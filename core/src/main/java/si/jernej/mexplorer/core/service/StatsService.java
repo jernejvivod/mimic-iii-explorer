905ccc05271ae100674b52ac36ad25a10ece69fc
Jernej Vivod <vivod.jernej@gmail.com>
2022-03-10 18:44:53 +0100

    Add prototype functionality

---
+++ core/src/main/java/si/jernej/mexplorer/core/service/StatsService.java
@@ -0,0 +1,22 @@
+package si.jernej.mexplorer.core.service;
+
+import java.util.List;
+
+import javax.enterprise.context.Dependent;
+
+import si.jernej.mexplorer.processorapi.v1.model.ColumnStatsDto;
+
+@Dependent
+public class StatsService
+{
+
+    public List<ColumnStatsDto> allStats()
+    {
+        return null;
+    }
+
+    public List<ColumnStatsDto> tableStats()
+    {
+        return null;
+    }
+}
