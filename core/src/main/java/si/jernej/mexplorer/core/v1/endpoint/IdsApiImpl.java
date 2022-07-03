e4cd1a88b14b78bb33a9e7aa6e2e760b17343ff1
Jernej Vivod <vivod.jernej@gmail.com>
2022-07-03 12:46:53 +0200

    [feat][#47] Add logging

--- core/src/main/java/si/jernej/mexplorer/core/v1/endpoint/IdsApiImpl.java
+++ core/src/main/java/si/jernej/mexplorer/core/v1/endpoint/IdsApiImpl.java
@@ -6,6 +6,9 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import si.jernej.mexplorer.core.processing.IdRetrieval;
 import si.jernej.mexplorer.processorapi.v1.api.IdsApi;
 import si.jernej.mexplorer.processorapi.v1.model.IdRetrievalSpecDto;
@@ -13,12 +16,15 @@ import si.jernej.mexplorer.processorapi.v1.model.IdRetrievalSpecDto;
 @Stateless
 public class IdsApiImpl implements IdsApi
 {
+    private static final Logger logger = LoggerFactory.getLogger(IdsApiImpl.class);
+
     @Inject
     private IdRetrieval idRetrieval;
 
     @Override
     public Response ids(@Valid @NotNull IdRetrievalSpecDto idRetrievalSpecDto)
     {
+        logger.info("extracting IDs");
         return Response.ok().entity(idRetrieval.retrieveIds(idRetrievalSpecDto)).build();
     }
 }
